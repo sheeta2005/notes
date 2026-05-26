7babff79b6d38990a4589a00712e1f8377161499
sheeta1998 <2169298883@qq.com>
2026-05-26 19:01:48 +0800

    rotate

---
+++ Algorithm/Rotate/Solution.java
@@ -0,0 +1,24 @@
+package Rotate;
+
+class Solution {
+    public void rotate(int[][] matrix) {
+
+        int n = matrix.length;
+        //转置
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j <n ; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        //垂直镜像
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <n/2 ; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[i][n-1-j];
+                matrix[i][n-1-j]=temp;
+            }
+        }
+    }
+}
